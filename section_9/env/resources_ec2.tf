1845bdcdf4e4575faab510ac99ad8706f5c3248c
FATESAIKOU <qzsecftbhhhh@gmail.com>
2021-09-01 17:07:45 +0900

    Remove redundant data in section_9 resource_ec2.tf

--- section_9/env/resources_ec2.tf
+++ section_9/env/resources_ec2.tf
@@ -4,26 +4,6 @@
 
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "aws-linux" {
-    most_recent = true
-    owners      = ["amazon"]
-
-    filter {
-        name   = "name"
-        values = ["amzn-ami-hvm*"]
-    }
-
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
 data "aws_ami" "ubuntu" {
     most_recent = true
 
